e27be696520112e1ef25f5332d1d592029e1ce0f
Conrad <conradh5656@gmail.com>
2022-04-26 12:43:01 +0100

    added didi's files

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-    require_once "credentials.php";
+    include "credentials.php";
     session_start();
 
     echo <<<_END
@@ -10,13 +10,103 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Movie Database</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel='stylesheet' id='stylesheet' href='css/stylesheet.css'"
+        <link rel='stylesheet' id='stylesheet' href='css/stylesheet.css'">
         </head>
     
         <body>
 
             <header>
-                <h1>Movie Database</h1>
+                <h1>Revived Blockbuster</h1>
             </header>
     _END;
+    $navigation = "";
+    if(isset($_SESSION['loggedIn']))
+    {
+        if ($_SESSION['username'] == 'admin')
+            {
+                //Admin prviledges
+                /*
+                *manage users
+                */
+                $navigation = 
+                "
+                <body>
+                    <nav class='navbar navbar-expand-sm bg-warning justify-content-center'>
+                            <ul class='navbar-nav'>
+                                <li class='nav-item'>
+                                    <a class='nav-link' href='index.php'>Home</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link' href='.php'>Create Movie</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link' href='.php'>Manage All Movies</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link' href='.php'>Manage Users</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link' href='.php'>Sign-Out ({$_SESSION['username']})</a>
+                                </li>
+                            </ul>
+                    </nav>
+                ";
+
+            }
+            else
+            {
+                $navigation = 
+                "
+                <body>
+                    <nav class='navbar navbar-expand-sm bg-dark justify-content-center'>
+                        <div class='container-fluid'>
+                        <ul class='navbar-nav'>
+                            <li class='nav-item'>
+                                <a class='nav-link' href='index.php'>Home</a>
+                            </li>            
+                            <li class='nav-item'>
+                                <a class='nav-link' href='.php'>Manage Movies</a>
+                            </li>
+                            <li class='nav-item'>
+                            <a class='nav-link' href='search.php'>Search</a>
+                            </li>
+                            <li class='nav-item'>
+                                <a class='nav-link' href='.php'>Sign-Out ({$_SESSION['username']})</a>
+                            </li>
+                    </ul>
+                </div>
+            </nav>
+                ";
+
+            }
+
+    }
+    else
+    {
+        $navigation = 
+        "
+        <body>
+        <nav class='navbar navbar-expand-sm bg-dark justify-content-center'>
+            <div class='container-fluid'>
+                <ul class='navbar-nav'>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='index.php'>Home</a>
+                    </li>
+                    <li class='nav-item'>
+                    <a class='nav-link' href='search.php'>Search</a>
+                   </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='signup_screen.php'>Sign-Up</a>
+                    </li>
+                    <li class='nav-item'>
+                        <a class='nav-link' href='login_screen.php'>Sign-In</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        ";
+
+    }
+    echo $navigation;
+
 ?>
--- index.php
+++ index.php
@@ -2,11 +2,50 @@
 
     include('header.php');
 
+    #SQL STUFF
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if (!$connection)
+    {
+        die("Connection failed: " . $mysqli_connection_error);
+    }
+
+    $query = "SELECT title, poster_path, vote_average, release_date, movie_id FROM movies WHERE status='Released' LIMIT 3";
+    $result = mysqli_query($connection, $query);
+    $n = mysqli_num_rows($result);
+
     echo <<<_END
-        <p>Example Content</p>
+    <div>
+    <h2>Newly released</h2>
+    </div>
     _END;
+
+    if ($n > 0)
+    {
+        while($rows = mysqli_fetch_assoc($result))
+        {
+            $imageDIR = "https://image.tmdb.org/t/p/original" . "{$rows['poster_path']}";
+            //echo posts?
+            echo <<<_END
+            <div class="post-container index-page ">
+            <form action="movie_Detail.php" method="post">
+                <button type="submit" name="showMovie" value="{$rows['movie_id']}" style="border:none;outline:none;background:none;"><img src="$imageDIR" width="180" height="270" style="border-radius: 5%;"/></button>
+            </form>
+            <h3>{$rows['vote_average']}</h3>
+            <h3>{$rows['title']}</h3>
+            <h4>{$rows['release_date']}</h4>
+            </div>
+            _END;
+        }
+    }
+    else
+    {
+        echo <<<_END
+        <h3>No posts found</h3>
+        _END;
+    }
 
     include('footer.php');
     echo"</body></html>";
-
 ?>
