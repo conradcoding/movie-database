3f221278fd3b6e677bc97a7960cc6b4411f90668
Conrad <conradh5656@gmail.com>
2022-04-26 17:26:59 +0100

    Linked details page with Didi's Index page

--- index.php
+++ index.php
@@ -11,30 +11,65 @@
         die("Connection failed: " . $mysqli_connection_error);
     }
 
-    $query = "SELECT title, poster_path, vote_average, release_date, movie_id FROM movies WHERE status='Released' LIMIT 3";
-    $result = mysqli_query($connection, $query);
-    $n = mysqli_num_rows($result);
+    $queryNR = "SELECT title, poster_path, vote_average, release_date, movie_id FROM movies WHERE status='Released' LIMIT 3";
+    $resultNR = mysqli_query($connection, $queryNR);
+    $nNR = mysqli_num_rows($resultNR);
 
     echo <<<_END
     <div>
-    <h2>Newly released</h2>
+    <h1>New releases</h1>
     </div>
     _END;
 
-    if ($n > 0)
+    if ($nNR > 0)
     {
-        while($rows = mysqli_fetch_assoc($result))
+        while($rows = mysqli_fetch_assoc($resultNR))
         {
             $imageDIR = "https://image.tmdb.org/t/p/original" . "{$rows['poster_path']}";
             //echo posts?
             echo <<<_END
-            <div class="post-container index-page ">
-            <form action="movie_Detail.php" method="post">
+            <div>
+            <form action="details.php" method="post">
                 <button type="submit" name="showMovie" value="{$rows['movie_id']}" style="border:none;outline:none;background:none;"><img src="$imageDIR" width="180" height="270" style="border-radius: 5%;"/></button>
             </form>
-            <h3>{$rows['vote_average']}</h3>
-            <h3>{$rows['title']}</h3>
-            <h4>{$rows['release_date']}</h4>
+            <h2>{$rows['vote_average']}</h3>
+            <h2>{$rows['title']}</h3>
+            <h3>{$rows['release_date']}</h4>
+            </div>
+            _END;
+        }
+    }
+    else
+    {
+        echo <<<_END
+        <h3>No posts found</h3>
+        _END;
+    }
+
+    $queryPP = "SELECT title, poster_path, vote_average, release_date, movie_id FROM movies WHERE vote_average > 7 ORDER BY vote_count DESC LIMIT 3";
+    $resultPP = mysqli_query($connection, $queryPP);
+    $nPP = mysqli_num_rows($resultPP);
+
+    echo <<<_END
+    <div>
+    <h1>Popular</h1>
+    </div>
+    _END;
+
+    if ($nPP > 0)
+    {
+        while($rows = mysqli_fetch_assoc($resultPP))
+        {
+            $imageDIR = "https://image.tmdb.org/t/p/original" . "{$rows['poster_path']}";
+            //echo posts?
+            echo <<<_END
+            <div>
+            <form action="details.php" method="post">
+                <button type="submit" name="showMovie" value="{$rows['movie_id']}" style="border:none;outline:none;background:none;"><img src="$imageDIR" width="180" height="270" style="border-radius: 5%;"/></button>
+            </form>
+            <h2>{$rows['vote_average']}</h3>
+            <h2>{$rows['title']}</h3>
+            <h3>{$rows['release_date']}</h4>
             </div>
             _END;
         }
